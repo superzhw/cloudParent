42c824e78b0842da9b63ba841776d0f1daad5fd5
小m仕 <zhanghw_zhangsh@126.com>
2021-06-01 18:33:23 +0800

    端口修改

--- cloud-order8002/src/main/java/com/taiji/springcloud/OrderMain8002.java
+++ cloud-order8002/src/main/java/com/taiji/springcloud/OrderMain8002.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrderMain {
+public class OrderMain8002 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain.class,args);
-        System.out.println("8082启动成功");
+        SpringApplication.run(OrderMain8002.class,args);
+        System.out.println("8002启动成功");
     }
 }
