9308dfffab62238013bbc0db9bffa865ce0e7592
小m仕 <zhanghw_zhangsh@126.com>
2021-06-07 10:16:53 +0800

    ribbon负载均衡方式把轮询修改成随机方式

--- cloud-order8002/src/main/java/com/taiji/springcloud/EurekaOrderMain8002.java
+++ cloud-order8002/src/main/java/com/taiji/springcloud/EurekaOrderMain8002.java
@@ -1,11 +1,14 @@
 package com.taiji.springcloud;
 
+import com.taiji.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "CLOUD-PROVIDER-SERVICE", configuration = MySelfRule.class)
 public class EurekaOrderMain8002 {
     public static void main(String[] args) {
         SpringApplication.run(EurekaOrderMain8002.class,args);
