d53b02b22a5266ff3fb8d95d12523b1d976d3666
小m仕 <zhanghw_zhangsh@126.com>
2021-06-07 10:21:01 +0800

    ribbon负载均衡方式把轮询修改成随机方式

--- cloud-order8002/src/main/java/com/taiji/myrule/MySelfRule.java
+++ cloud-order8002/src/main/java/com/taiji/myrule/MySelfRule.java
@@ -5,6 +5,9 @@ import com.netflix.loadbalancer.RandomRule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * 不能和启动类在同一个包下，会影响其他
+ */
 @Configuration
 public class MySelfRule {
     @Bean
