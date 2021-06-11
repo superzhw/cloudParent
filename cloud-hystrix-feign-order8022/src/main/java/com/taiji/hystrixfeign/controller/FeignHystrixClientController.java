31ea291f4de4a3e306fc4b8aa6b9c56670971a5d
小m仕 <zhanghw_zhangsh@126.com>
2021-06-11 15:59:35 +0800

    消费端集成hystrix，并配置全局降级执行方法

--- cloud-hystrix-feign-order8022/src/main/java/com/taiji/hystrixfeign/controller/FeignHystrixClientController.java
+++ cloud-hystrix-feign-order8022/src/main/java/com/taiji/hystrixfeign/controller/FeignHystrixClientController.java
@@ -1,5 +1,6 @@
 package com.taiji.hystrixfeign.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.taiji.hystrixfeign.common.Result;
@@ -7,10 +8,12 @@ import com.taiji.hystrixfeign.service.FeignClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
+@DefaultProperties(defaultFallback = "globalFallbackMethod")
 public class FeignHystrixClientController {
 
     @Autowired
@@ -42,5 +45,28 @@ public class FeignHystrixClientController {
 
     public  Result paymentInfoTimeoutHandler(Integer id){
         return new Result(444,"8022消费端服务降级","paymentInfoTimeoutHandler线程池： " + Thread.currentThread().getName() + "  {}{}id:   "+id);
+    }
+
+    /**
+     * 延迟方法
+     * @param id
+     * @return
+     */
+    @GetMapping("/hystrix/globe/{id}")
+    @HystrixCommand
+    public Result paymentInfoTimeout(@PathVariable("id") Integer id){
+//        try {
+//            TimeUnit.SECONDS.sleep(3);
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+        int a = 10/0;
+        return new Result(444,"8022消费端全局正常返回",Thread.currentThread().getName());
+    }
+
+    // 下面是全局fallback方法
+    public Result globalFallbackMethod()
+    {
+        return new Result(444,"Global异常处理信息，请稍后再试，/(ㄒoㄒ)/~~","");
     }
 }
