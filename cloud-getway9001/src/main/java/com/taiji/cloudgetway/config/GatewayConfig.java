c80cc757d7d738c5494f651433481f5ced419b1f
小m仕 <zhanghw_zhangsh@126.com>
2021-06-22 17:28:42 +0800

    gateway路由的配置类

---
+++ cloud-getway9001/src/main/java/com/taiji/cloudgetway/config/GatewayConfig.java
@@ -0,0 +1,24 @@
+package com.taiji.cloudgetway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+    /**
+     * 配置一个id为route-name的路由规则
+     * 当访问http://localhost/guonei时会自动转发到地址：https://news.baidu.com/guonei
+     * @param routeLocatorBuilder
+     * @return
+     */
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder){
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        routes.route("path_route1",
+                r -> r.path("/guonei")
+                        .uri("http://news.baidu.com/guonei")).build();
+        return routes.build();
+    }
+}
