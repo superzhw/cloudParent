59e639bb76d0e1654ba45577b01f4a03d663e3de
小m仕 <zhanghw_zhangsh@126.com>
2021-06-23 15:17:28 +0800

    predicate网关断言配置

---
+++ cloud-getway9001/src/test/java/com/taiji/cloudgateway/T.java
@@ -0,0 +1,13 @@
+package com.taiji.cloudgateway;
+
+import org.junit.Test;
+
+import java.time.ZonedDateTime;
+
+public class T {
+    @Test
+    public void  aa() {
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now);
+    }
+}
